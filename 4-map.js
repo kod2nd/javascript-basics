3092db35afa7641a72cd6a07c21fc97f30c866e9
kod2nd <colinongengkiat@gmail.com>
2018-06-12 12:35:00 +0800

    Updated 4-map

--- 4-map.js
+++ 4-map.js
@@ -3,7 +3,7 @@ var assertEquals = require("./assert-helper");
 /* ---------------------- EXERCISE 0 ---------------------- */
 // define a function that takes in array of strings with every element converted to uppercase
 function convertToUpperCase(array) {
-
+  return array.map(string => string.toUpperCase())
 }
 
 
@@ -15,7 +15,7 @@ assertEquals(convertToUpperCase(["hello", "gOOdBYE"]), ["HELLO", "GOODBYE"]);
 /* ---------------------- EXERCISE 1 ---------------------- */
 // define a function that multiplies all elements in an array by 10
 function multiplyBy10(array) {
-
+  return array.map(num => num * 10)
 }
 
 
@@ -28,8 +28,10 @@ assertEquals(multiplyBy10([-1, 0, 1]), [-10, 0, 10]);
 /* ---------------------- EXERCISE 2 ---------------------- */
 // define a function that takes in an array objects and returns only an array of strings of the name field
 
-function onlyNames(array) {
 
+
+function onlyNames(array) {
+  return array.map(object => object['name'])
 }
 
 // Assertions (do not change)
@@ -52,8 +54,26 @@ assertEquals(onlyNames(input), ["homer", "marge", "lisa"]);
 
 /* ---------------------- EXERCISE 3 ---------------------- */
 // define a function that removes any non-vowel character from words in an array
+
+
+
+
+
+
 function keepOnlyVowels(array) {
-  
+
+  const isVowel = (string) => {
+    const newArray = []
+    for (letter of string) {
+      for (vowel of ['a', 'e', 'i', 'o', 'u']) {
+        if (vowel === letter) {
+          newArray.push(letter)
+        }
+      }
+    }
+    return newArray.join('')
+  }
+  return array.map(isVowel)
 }
 
 
